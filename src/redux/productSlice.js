14b46ebea1a21286ae50c48b960f812907d47ae5
PShivers <paulsshivers@gmail.com>
2023-05-13 10:41:51 -0400

    added product slice and store

---
+++ src/redux/productSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productSlice = createSlice({
+	name: "products",
+	initialState: [],
+	reducers: {},
+	extraReducers: {},
+});
+
+export default productSlice.reducer;
