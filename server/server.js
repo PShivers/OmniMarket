5042a38a4b5f5c2ad87d16bff55d4e469816d96d
PShivers <paulsshivers@gmail.com>
2023-05-13 10:44:31 -0400

    edited path of schema in server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 //runs a server at localhost 4000
 const { ApolloServer } = require("apollo-server");
 const fs = require("fs");
-const typeDefs = fs.readFileSync("./schema.graphql", { encoding: "utf-8" });
+const typeDefs = fs.readFileSync("../schema.graphql", { encoding: "utf-8" });
 const resolvers = require("../resolvers/resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
